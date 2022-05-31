d59409c7c47b2de1f9ff2d84f20aa82946472aca
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-05-31 23:58:21 +0100

    Refactoring Cloud Animations

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,6 +11,12 @@ const Home = ({ darkMode, setDarkMode }) => {
           <div className="cloud1 four"></div>
           <div className="cloud1 five"></div>
           <div className="cloud1 six"></div>
+          <div className="cloud2 seven"></div>
+          <div className="cloud2 eight"></div>
+          <div className="cloud2 nine"></div>
+          <div className="cloud3 ten"></div>
+          <div className="cloud4 eleven"></div>
+          <div className="cloud5 twelve"></div>
           <div className="home-content">
             <h1>Ben Mostyn</h1>
             <h2> A Creative Front-End Developer</h2>
