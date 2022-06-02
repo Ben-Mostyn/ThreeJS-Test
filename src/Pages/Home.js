191bdc04a424a18e9456a4d1ccc44097dcc396b9
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-06-03 00:49:14 +0100

    Cloud Changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,8 +4,8 @@ const Home = ({ darkMode, setDarkMode }) => {
   return (
     <div className="main-home-container">
       {darkMode ? (
-        <div className="Home-container">
-          <div className="cloud one"></div>
+        <section className="Home-container">
+          {/* <div className="cloud one"></div>
           <div className="cloud two"></div>
           <div className="cloud three"></div>
           <div className="cloud1 four"></div>
@@ -17,11 +17,25 @@ const Home = ({ darkMode, setDarkMode }) => {
           <div className="cloud3 ten"></div>
           <div className="cloud4 eleven"></div>
           <div className="cloud5 twelve"></div>
+          <div className="cloud6 thirteen"></div> */}
+          {/* <div className="container">
+            <div className="cloud"></div>
+          </div> */}
+          <div className="cloud1"></div>
+          <div className="cloud2"></div>
+          <div className="cloud3"></div>
+          <div className="cloud4 one"></div>
+          <div className="cloud4 two"></div>
+          <div className="cloud5 three"></div>
+          <div className="cloud5 four"></div>
+          <div className="cloud6 five"></div>
+          <div className="cloud6 six"></div>
+
           <div className="home-content">
             <h1>Ben Mostyn</h1>
             <h2> A Creative Front-End Developer</h2>
           </div>
-        </div>
+        </section>
       ) : (
         <div className="home-container-dark">
           <h1>Home</h1>
